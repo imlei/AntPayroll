277e0a580124fd9ad6f20d22def96ddd6bfc8410
imlei <huanglei@live.com>
2020-09-02 00:26:17 -0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+//help to calculate the payroll
+//Thanks for the CRA offer the usefull tools help me to build this software
+//https://www.canada.ca/en/revenue-agency/services/e-services/e-services-businesses/payroll-deductions-online-calculator.html
+//last update 20200901
+
+import "fmt"
+
+var (
+	grossamount float64
+	paytype     int
+	taxamount   float64
+	payfre      float64
+	fixamt1     float64
+	fixamt2     float64
+	fixamt3     float64
+	fixamt4     float64
+	cppamt      float64
+	eiamt       float64
+)
+
+func main() {
+
+	var incomeclass = map[string]float64{
+		"level1": 48535.01,
+		"level2": 97069.01,
+		"level3": 150473.01,
+		"level4": 214368.01,
+		"rate1":  0.15,
+		"rate2":  0.205,
+		"rate3":  0.26,
+		"rate4":  0.29,
+		"rate5":  0.33,
+		"fbpa":   13229.00,
+		"fbpac":  1810.35,
+		"kcpp":   0.0525,
+		"kei":    0.0158,
+	}
+	fmt.Println("Please enter your payroll:")
+	fmt.Scanln(&grossamount)
+	fmt.Println("Please enter the year of income:/n 1 = monthly; 2 = Semi-monthly; 3 = bi-weekly; 4 = weekly; 5 = daily")
+	fmt.Scanln(&paytype)
+	//choose pay period frequency
+	if paytype == 1 {
+		payfre = 12
+	} else if paytype == 2 {
+		payfre = 24
+	} else if paytype == 3 {
+		payfre = 26
+	} else if paytype == 4 {
+		payfre = 52
+	} else {
+		payfre = 260
+	}
+
+	//Calculate the basic tax
+	grossamount = grossamount * payfre
+	//15% on the first $48,535 of taxable income
+	fixamt1 = incomeclass["level1"] * incomeclass["rate1"]
+	//20.5% on the portion of taxable income over $48,535 up to $97,069
+	fixamt2 = (incomeclass["level2"]-incomeclass["level1"])*incomeclass["rate2"] + fixamt1
+	//26% on the portion of taxable income over $97,069 up to $150,473
+	fixamt3 = (incomeclass["level3"]-incomeclass["level2"])*incomeclass["rate3"] + fixamt2
+	//29% on the portion of taxable income over $150,473 up to $214,368
+	fixamt4 = (incomeclass["level4"]-incomeclass["level3"])*incomeclass["rate4"] + fixamt3
+	//33% of taxable income over $214,368
+
+	//calculate the Federal income tax payable
+	if grossamount < incomeclass["level1"] {
+		taxamount = grossamount * incomeclass["rate1"]
+	} else if grossamount < incomeclass["level2"] {
+		taxamount = (grossamount-incomeclass["level1"])*incomeclass["rate2"] + fixamt1
+	} else if grossamount < incomeclass["level3"] {
+		taxamount = (grossamount-incomeclass["level2"])*incomeclass["rate3"] + fixamt2
+	} else if grossamount < incomeclass["level4"] {
+		taxamount = (grossamount-incomeclass["level3"])*incomeclass["rate4"] + fixamt3
+	} else {
+		taxamount = (grossamount-incomeclass["level4"])*incomeclass["rate5"] + fixamt4
+	}
+	//calculate the CPP and EI amount, need to update for max amount.
+	cppamt = grossamount * incomeclass["kcpp"] / payfre
+	eiamt = grossamount * incomeclass["kei"] / payfre
+	//less the federl personal amount
+	if grossamount > incomeclass["fbpa"] {
+		taxamount = (taxamount-incomeclass["fbpac"])/payfre - cppamt*0.15 - eiamt*0.15
+	} else {
+		taxamount = 0
+	}
+
+	fmt.Println("your gross amount:", grossamount)
+	fmt.Println("Income tax payable:", taxamount)
+	fmt.Println("CPP tax payable:", cppamt)
+	fmt.Println("EI tax payable:", eiamt)
+}
