5e9b6f32a01071c0b19bdf6f560ab01563ed77b3
imlei <huanglei@live.com>
2020-09-06 02:50:56 -0700

    update 20200906 missing SK
    Add most of the provincial, but missing QC. Also there are some basic credit amount information is missing.

--- main.go
+++ main.go
@@ -17,7 +17,14 @@ type Taxconfig struct {
 	Mbtax Mbtax `json:"mbtax"`
 	Mysql Mysql  `json:"mysql"`
 	Nbtax Nbtax `json:"nbtax"`
+	Nltax Nltax `json:"nltax"`
+	Nstax Nstax `json:"nstax"`
+	Nttax Nttax `json:"nttax"`
+	Nutax Nutax `json:"nutax"`
+	Ontax Ontax `json:"ontax"`
+	Petax Petax `json:"petax"`
 	Sktax Sktax  `json:"sktax"`
+	Yttax Yttax `json:"yttax"`
 }
 type Abtax struct {
 	L1 float64    `json:"l1"`
@@ -51,19 +58,100 @@ type Nbtax struct {
 	R5 float64 `json:"r5"`
 	Pba float64 `json："pba"`
 }
+type Nltax struct {
+	L1 float64    `json:"l1"`
+	L2 float64     `json:"l2"`
+	L3 float64    `json:"l3"`
+	L4 float64    `json:"l4"`
+	R1 float64 `json:"r1"`
+	R2 float64 `json:"r2"`
+	R3 float64 `json:"r3"`
+	R4 float64 `json:"r4"`
+	R5 float64 `json:"r5"`
+	Pba float64 `json："pba"`
+}
+type Nstax struct {
+	L1 float64    `json:"l1"`
+	L2 float64     `json:"l2"`
+	L3 float64    `json:"l3"`
+	L4 float64    `json:"l4"`
+	R1 float64 `json:"r1"`
+	R2 float64 `json:"r2"`
+	R3 float64 `json:"r3"`
+	R4 float64 `json:"r4"`
+	R5 float64 `json:"r5"`
+	Pba float64 `json："pba"`
+}
+type Nttax struct {
+	L1 float64    `json:"l1"`
+	L2 float64     `json:"l2"`
+	L3 float64    `json:"l3"`
+	R1 float64 `json:"r1"`
+	R2 float64 `json:"r2"`
+	R3 float64 `json:"r3"`
+	R4 float64 `json:"r4"`
+	Pba float64 `json："pba"`
+}
+type Nutax struct {
+	L1 float64    `json:"l1"`
+	L2 float64     `json:"l2"`
+	L3 float64    `json:"l3"`
+	R1 float64 `json:"r1"`
+	R2 float64 `json:"r2"`
+	R3 float64 `json:"r3"`
+	R4 float64 `json:"r4"`
+	Pba float64 `json："pba"`
+}
+type Ontax struct {
+	L1 float64    `json:"l1"`
+	L2 float64     `json:"l2"`
+	L3 float64    `json:"l3"`
+	L4 float64    `json:"l4"`
+	R1 float64 `json:"r1"`
+	R2 float64 `json:"r2"`
+	R3 float64 `json:"r3"`
+	R4 float64 `json:"r4"`
+	R5 float64 `json:"r5"`
+	Pba float64 `json："pba"`
+}
+type Petax struct {
+	L1 float64    `json:"l1"`
+	L2 float64     `json:"l2"`
+	R1 float64 `json:"r1"`
+	R2 float64 `json:"r2"`
+	R3 float64 `json:"r3"`
+	Pba float64 `json："pba"`
+}
+type Sktax struct {
+	L1 float64    `json:"l1"`
+	L2 float64     `json:"l2"`
+	R1 float64 `json:"r1"`
+	R2 float64 `json:"r2"`
+	R3 float64 `json:"r3"`
+	Pba float64 `json："pba"`
+}
+type Yttax struct {
+	L1 float64    `json:"l1"`
+	L2 float64     `json:"l2"`
+	L3 float64    `json:"l3"`
+	L4 float64    `json:"l4"`
+	R1 float64 `json:"r1"`
+	R2 float64 `json:"r2"`
+	R3 float64 `json:"r3"`
+	R4 float64 `json:"r4"`
+	R5 float64 `json:"r5"`
+	Pba float64 `json："pba"`
+}
 type Mysql struct {
 	URL      string `json:"url"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
-type Sktax struct {
-	L1 int `json:"l1"`
-}
 
 var (
 	grossamount    float64
 	pramt          float64 //prorate amount
-	paytype, ptinfo int
+	paytype int
 	taxamount      float64
 	payfre         float64
 	fixamt1        float64
@@ -74,7 +162,7 @@ var (
 	eiamt          float64
 	cecamt         float64
 	icamt, picamt  float64
-	v             string
+	v,ptinfo             string
 )
 
 var incomeclass = map[string]float64{
@@ -134,12 +222,37 @@ func main() {
 	}
 
 	pramt = grossamount * payfre
-
-	if ptinfo == 1 {
+switch {
+case ptinfo == "AB":
+	picamt = ptab(pramt)/payfre
+    case ptinfo == "ON":
+	picamt = pton(pramt)/payfre
+	case ptinfo == "NU":
+		picamt = ptnu(pramt)/payfre
+case ptinfo == "BC":
+	picamt = ptbc(pramt)/payfre
+case ptinfo == "MB":
+	picamt = ptmb(pramt)/payfre
+case ptinfo == "NB":
+	picamt = ptnb(pramt)/payfre
+case ptinfo == "NL":
+	picamt = ptnl(pramt)/payfre
+case ptinfo == "NS":
+	picamt = ptns(pramt)/payfre
+case ptinfo == "NT":
+	picamt = ptnt(pramt)/payfre
+case ptinfo == "PE":
+	picamt = ptpe(pramt)/payfre
+case ptinfo == "SK":
+	picamt = ptsk(pramt)/payfre
+case ptinfo == "YT":
+	picamt = ptyt(pramt)/payfre
+}
+	if ptinfo == "AB" {
 		 picamt = ptab(pramt)/payfre
-	} else if ptinfo == 2{
+	} else if ptinfo == "BC"{
 		picamt = ptbc(pramt)/payfre
-	} else if ptinfo == 3 {
+	} else if ptinfo == "MB" {
 		picamt = ptmb(pramt)/payfre
 	} else {
 		picamt = ptnb(pramt)/payfre
@@ -201,7 +314,7 @@ func ReadJsonFile() {
 	var JsonParse = NewJsonStruct()
 	v := Taxconfig{}
 	//下面使用的是相对路径
-	JsonParse.Load("./core/config.json", &v)
+	JsonParse.Load("./config/config.json", &v)
 }
 
 type JsonStruct struct {
@@ -325,7 +438,7 @@ func ptab(ab float64) float64 {
 	var JsonParse = NewJsonStruct()
 	v := Taxconfig{}
 	//下面使用的是相对路径
-	JsonParse.Load("./core/config.json", &v)
+	JsonParse.Load("./config/config.json", &v)
 
 	//read config file
 
@@ -368,7 +481,7 @@ func ptmb(mb float64) float64 {
 	var JsonParse = NewJsonStruct()
 	v := Taxconfig{}
 	//下面使用的是相对路径
-	JsonParse.Load("./core/config.json", &v)
+	JsonParse.Load("./config/config.json", &v)
 
 	//read config file
 
@@ -403,7 +516,7 @@ func ptnb(nb float64) float64 {
 	var JsonParse = NewJsonStruct()
 	v := Taxconfig{}
 	//下面使用的是相对路径
-	JsonParse.Load("./core/config.json", &v)
+	JsonParse.Load("./config/config.json", &v)
 
 	//read config file
 
@@ -437,4 +550,318 @@ func ptnb(nb float64) float64 {
 	} else {
 	}
 	return nb
+}
+func ptnl(nl float64) float64 {
+	var (
+		nlamt1, nlamt2, nlamt3, nlamt4 float64
+	)
+	var JsonParse = NewJsonStruct()
+	v := Taxconfig{}
+	//下面使用的是相对路径
+	JsonParse.Load("./config/config.json", &v)
+
+	//read config file
+
+	//first $131,220
+	nlamt1 = v.Nltax.R1 * v.Nltax.L1
+	//over $131,220 to $ 157,464
+	nlamt2 = (v.Nltax.L2-v.Nltax.L1)*v.Nltax.R2 + nlamt1
+	//over $157,464 to $209,950
+	nlamt3 = (v.Nltax.L3-v.Nltax.L2)*v.Nltax.R3 + nlamt2
+	//over $209,950 to $314,928
+	nlamt4 = (v.Nltax.L4-v.Nltax.L3)*v.Nltax.R4 + nlamt3
+
+
+	//calculate the Federal income tax paynlle
+
+	if pramt < v.Nltax.L1 {
+		nl = pramt * v.Nltax.R1
+	} else if pramt < v.Nltax.L2 {
+		nl = (pramt-v.Nltax.L1)*v.Nltax.R2 + nlamt1
+	} else if pramt < v.Nltax.L3 {
+		nl = (pramt-v.Nltax.L2)*v.Nltax.R3 + nlamt2
+	} else if pramt < v.Nltax.L4 {
+		nl = (pramt-v.Nltax.L3)*v.Nltax.R4 + nlamt3
+	} else {
+		nl = (pramt-v.Nltax.L4)*v.Nltax.R5 + nlamt4
+	}
+
+	nl = nl - v.Nltax.Pba * v.Nltax.R1 - cpp(pramt)*v.Nltax.R1 - ei(pramt)*v.Nltax.R1
+	if nl <= 0 {
+		nl = 0
+	} else {
+	}
+	return nl
+}
+func ptns(ns float64) float64 {
+	var (
+		nsamt1, nsamt2, nsamt3, nsamt4 float64
+	)
+	var JsonParse = NewJsonStruct()
+	v := Taxconfig{}
+	//下面使用的是相对路径
+	JsonParse.Load("./config/config.json", &v)
+
+	//read config file
+
+	//first $131,220
+	nsamt1 = v.Nstax.R1 * v.Nstax.L1
+	//over $131,220 to $ 157,464
+	nsamt2 = (v.Nstax.L2-v.Nstax.L1)*v.Nstax.R2 + nsamt1
+	//over $157,464 to $209,950
+	nsamt3 = (v.Nstax.L3-v.Nstax.L2)*v.Nstax.R3 + nsamt2
+	//over $209,950 to $314,928
+	nsamt4 = (v.Nstax.L4-v.Nstax.L3)*v.Nstax.R4 + nsamt3
+
+
+	//calculate the Federal income tax paynsle
+
+	if pramt < v.Nstax.L1 {
+		ns = pramt * v.Nstax.R1
+	} else if pramt < v.Nstax.L2 {
+		ns = (pramt-v.Nstax.L1)*v.Nstax.R2 + nsamt1
+	} else if pramt < v.Nstax.L3 {
+		ns = (pramt-v.Nstax.L2)*v.Nstax.R3 + nsamt2
+	} else if pramt < v.Nstax.L4 {
+		ns = (pramt-v.Nstax.L3)*v.Nstax.R4 + nsamt3
+	} else {
+		ns = (pramt-v.Nstax.L4)*v.Nstax.R5 + nsamt4
+	}
+
+	ns = ns - v.Nstax.Pba * v.Nstax.R1 - cpp(pramt)*v.Nstax.R1 - ei(pramt)*v.Nstax.R1
+	if ns <= 0 {
+		ns = 0
+	} else {
+	}
+	return ns
+}
+func ptnt(nt float64) float64 {
+	var (
+		ntamt1, ntamt2, ntamt3 float64
+	)
+	var JsonParse = NewJsonStruct()
+	v := Taxconfig{}
+	//下面使用的是相对路径
+	JsonParse.Load("./cntfig/cntfig.jsnt", &v)
+
+	//read cntfig file
+
+	//first $131,220
+	ntamt1 = v.Nttax.R1 * v.Nttax.L1
+	//over $131,220 to $ 157,464
+	ntamt2 = (v.Nttax.L2-v.Nttax.L1)*v.Nttax.R2 + ntamt1
+	//over $157,464 to $209,950
+	ntamt3 = (v.Nttax.L3-v.Nttax.L2)*v.Nttax.R3 + ntamt2
+
+
+
+	//calculate the Federal income tax payntle
+
+	if pramt < v.Nttax.L1 {
+		nt = pramt * v.Nttax.R1
+	} else if pramt < v.Nttax.L2 {
+		nt = (pramt-v.Nttax.L1)*v.Nttax.R2 + ntamt1
+	} else if pramt < v.Nttax.L3 {
+		nt = (pramt-v.Nttax.L2)*v.Nttax.R3 + ntamt2
+	} else  {
+		nt = (pramt-v.Nttax.L3)*v.Nttax.R4 + ntamt3
+	}
+
+	nt = nt - v.Nttax.Pba * v.Nttax.R1 - cpp(pramt)*v.Nttax.R1 - ei(pramt)*v.Nttax.R1
+	if nt <= 0 {
+		nt = 0
+	} else {
+	}
+	return nt
+}
+func ptnu(nu float64) float64 {
+	var (
+		nuamt1, nuamt2, nuamt3 float64
+	)
+	var JsonParse = NewJsonStruct()
+	v := Taxconfig{}
+	//下面使用的是相对路径
+	JsonParse.Load("./cnufig/cnufig.jsnu", &v)
+
+	//read cnufig file
+
+	//first $131,220
+	nuamt1 = v.Nutax.R1 * v.Nutax.L1
+	//over $131,220 to $ 157,464
+	nuamt2 = (v.Nutax.L2-v.Nutax.L1)*v.Nutax.R2 + nuamt1
+	//over $157,464 to $209,950
+	nuamt3 = (v.Nutax.L3-v.Nutax.L2)*v.Nutax.R3 + nuamt2
+	//over $209,950 to $314,928
+
+
+	//calculate the Federal income tax paynule
+
+	if pramt < v.Nutax.L1 {
+		nu = pramt * v.Nutax.R1
+	} else if pramt < v.Nutax.L2 {
+		nu = (pramt-v.Nutax.L1)*v.Nutax.R2 + nuamt1
+	} else if pramt < v.Nutax.L3 {
+		nu = (pramt-v.Nutax.L2)*v.Nutax.R3 + nuamt2
+	} else  {
+		nu = (pramt-v.Nutax.L3)*v.Nutax.R4 + nuamt3
+	}
+
+	nu = nu - v.Nutax.Pba * v.Nutax.R1 - cpp(pramt)*v.Nutax.R1 - ei(pramt)*v.Nutax.R1
+	if nu <= 0 {
+		nu = 0
+	} else {
+	}
+	return nu
+}
+func pton(on float64) float64 {
+	var (
+		onamt1, onamt2, onamt3, onamt4 float64
+	)
+	var JsonParse = NewJsonStruct()
+	v := Taxconfig{}
+	//下面使用的是相对路径
+	JsonParse.Load("./config/config.json", &v)
+
+	//read config file
+
+	//first $131,220
+	onamt1 = v.Ontax.R1 * v.Ontax.L1
+	//over $131,220 to $ 157,464
+	onamt2 = (v.Ontax.L2-v.Ontax.L1)*v.Ontax.R2 + onamt1
+	//over $157,464 to $209,950
+	onamt3 = (v.Ontax.L3-v.Ontax.L2)*v.Ontax.R3 + onamt2
+	//over $209,950 to $314,928
+	onamt4 = (v.Ontax.L4-v.Ontax.L3)*v.Ontax.R4 + onamt3
+
+
+	//calculate the Federal income tax payonle
+
+	if pramt < v.Ontax.L1 {
+		on = pramt * v.Ontax.R1
+	} else if pramt < v.Ontax.L2 {
+		on = (pramt-v.Ontax.L1)*v.Ontax.R2 + onamt1
+	} else if pramt < v.Ontax.L3 {
+		on = (pramt-v.Ontax.L2)*v.Ontax.R3 + onamt2
+	} else if pramt < v.Ontax.L4 {
+		on = (pramt-v.Ontax.L3)*v.Ontax.R4 + onamt3
+	} else {
+		on = (pramt-v.Ontax.L4)*v.Ontax.R5 + onamt4
+	}
+
+	on = on - v.Ontax.Pba * v.Ontax.R1 - cpp(pramt)*v.Ontax.R1 - ei(pramt)*v.Ontax.R1
+	if on <= 0 {
+		on = 0
+	} else {
+	}
+	return on
+}
+func ptpe(pe float64) float64 {
+	var (
+		peamt1, peamt2 float64
+	)
+	var JsonParse = NewJsonStruct()
+	v := Taxconfig{}
+	//下面使用的是相对路径
+	JsonParse.Load("./config/config.json", &v)
+
+	//read config file
+
+	//first $33,389
+	peamt1 = v.Petax.R1 * v.Petax.L1
+	//over $33,389 to $ 72,164
+	peamt2 = (v.Petax.L2-v.Petax.L1)*v.Petax.R2 + peamt1
+
+
+	//calculate the Federal income tax paypele
+
+	if pramt < v.Petax.L1 {
+		pe = pramt * v.Petax.R1
+	} else if pramt < v.Petax.L2 {
+		pe = (pramt-v.Petax.L1)*v.Petax.R2 + peamt1
+	} else {
+		pe = (pramt-v.Petax.L2)*v.Petax.R3 + peamt2
+	}
+
+	pe = pe - v.Petax.Pba * v.Petax.R1 - cpp(pramt)*v.Petax.R1 - ei(pramt)*v.Petax.R1
+	if pe <= 0 {
+		pe = 0
+	} else {
+	}
+	return pe
+}
+func ptsk(sk float64) float64 {
+	var (
+		skamt1, skamt2 float64
+	)
+	var JsonParse = NewJsonStruct()
+	v := Taxconfig{}
+	//下面使用的是相对路径
+	JsonParse.Load("./config/config.json", &v)
+
+	//read config file
+
+	//first $33,389
+	skamt1 = v.Sktax.R1 * v.Sktax.L1
+	//over $33,389 to $ 72,164
+	skamt2 = (v.Sktax.L2-v.Sktax.L1)*v.Sktax.R2 + skamt1
+
+
+	//calculate the Federal income tax payskle
+
+	if pramt < v.Sktax.L1 {
+		sk = pramt * v.Sktax.R1
+	} else if pramt < v.Sktax.L2 {
+		sk = (pramt-v.Sktax.L1)*v.Sktax.R2 + skamt1
+	} else {
+		sk = (pramt-v.Sktax.L2)*v.Sktax.R3 + skamt2
+	}
+
+	sk = sk - v.Sktax.Pba * v.Sktax.R1 - cpp(pramt)*v.Sktax.R1 - ei(pramt)*v.Sktax.R1
+	if sk <= 0 {
+		sk = 0
+	} else {
+	}
+	return sk
+}
+func ptyt(yt float64) float64 {
+	var (
+		ytamt1, ytamt2, ytamt3, ytamt4 float64
+	)
+	var JsonParse = NewJsonStruct()
+	v := Taxconfig{}
+	//下面使用的是相对路径
+	JsonParse.Load("./cytfig/cytfig.jsyt", &v)
+
+	//read cytfig file
+
+	//first $131,220
+	ytamt1 = v.Yttax.R1 * v.Yttax.L1
+	//over $131,220 to $ 157,464
+	ytamt2 = (v.Yttax.L2-v.Yttax.L1)*v.Yttax.R2 + ytamt1
+	//over $157,464 to $209,950
+	ytamt3 = (v.Yttax.L3-v.Yttax.L2)*v.Yttax.R3 + ytamt2
+	//over $209,950 to $314,928
+	ytamt4 = (v.Yttax.L4-v.Yttax.L3)*v.Yttax.R4 + ytamt3
+
+
+	//calculate the Federal income tax payytle
+
+	if pramt < v.Yttax.L1 {
+		yt = pramt * v.Yttax.R1
+	} else if pramt < v.Yttax.L2 {
+		yt = (pramt-v.Yttax.L1)*v.Yttax.R2 + ytamt1
+	} else if pramt < v.Yttax.L3 {
+		yt = (pramt-v.Yttax.L2)*v.Yttax.R3 + ytamt2
+	} else if pramt < v.Yttax.L4 {
+		yt = (pramt-v.Yttax.L3)*v.Yttax.R4 + ytamt3
+	} else {
+		yt = (pramt-v.Yttax.L4)*v.Yttax.R5 + ytamt4
+	}
+
+	yt = yt - v.Yttax.Pba * v.Yttax.R1 - cpp(pramt)*v.Yttax.R1 - ei(pramt)*v.Yttax.R1
+	if yt <= 0 {
+		yt = 0
+	} else {
+	}
+	return yt
 }
